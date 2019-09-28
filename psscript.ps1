de7e2f3f2a7ac54ab5698f242c3461394eff3ad4
Victor Santana <victor@hepoca.com>
2019-09-28 14:24:22 -0500

    Add Windows

---
+++ psscript.ps1
@@ -0,0 +1,57 @@
+param (
+    [string]$InputParameters
+)
+
+# Creating InstallDir
+$Downloaddir = "C:\InstallDir"
+if ((Test-Path -Path $Downloaddir) -ne $true) {
+    mkdir $Downloaddir
+}
+
+Start-Transcript ($Downloaddir+".\InstallPSScript.log")
+
+function Log($Message){
+    Write-Output (([System.DateTime]::Now).ToString() + " " + $Message)
+}
+
+Log("##########################")
+Log("# Installing VSCode")
+Log("##########################")
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#$url = "https://aka.ms/win32-x64-user-stable"
+#$url = "https://vscode-update.azurewebsites.net/latest/win32-x64-user/stable"
+#$url = "https://go.microsoft.com/fwlink/?Linkid=852157"
+$url = "https://vscode-update.azurewebsites.net/latest/win32-x64/stable"
+
+Log("Downloading VSCode from $url to VSCodeSetup.exe")
+Invoke-WebRequest -Uri $url -OutFile ($Downloaddir+"\VSCodeSetup.exe")
+Log("Installing VSCode Using the command: $Downloaddir\VSCodeSetup.exe /verysilent /suppressmsgboxes /mergetasks=!runcode")
+(Start-Process ($Downloaddir+"\VSCodeSetup.exe") '/verysilent /suppressmsgboxes /mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath' -Wait -Passthru).ExitCode
+Log("Installing VSCode Extensions")
+code --install-extension ms-vscode.powershell -force
+code --install-extension ms-azuretools.vscode-docker -force
+code --install-extension ms-vscode.csharp -force
+code --install-extension ms-python.python -force
+code --install-extension vscode-icons-team.vscode-icons -force
+code --install-extension visualstudioexptteam.vscodeintellicode -force
+code --install-extension vscjava.vscode-maven -force
+code --install-extension vscjava.vscode-spring-boot-dashboard -force
+code --install-extension pivotal.vscode-spring-boot -force
+code --install-extension vscjava.vscode-spring-initializr -force
+code --install-extension vscjava.vscode-java-debug -force
+
+Log("##########################")
+Log("# Setting Windows Features")
+Log("##########################")
+Log("Disable IE ESC")
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+}
+
+Disable-InternetExplorerESC
+
+Log("Windows Firewall Allow Ping")
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
